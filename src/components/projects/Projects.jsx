8de791be35439455af75ef1ac2ab8ddc17e36777
gi-colucci <gcolucci2828@gmail.com>
2024-05-01 10:27:04 -0300

    ultimas atualizações

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,6 +2,7 @@ import './Projects.scss'
 import img from '../../assets/Rectangle 44.png';
 
 export function Projects() {
+  
     return (
       <>
       <h1 id="projects"></h1>
@@ -13,26 +14,44 @@ export function Projects() {
             <h1 className='title-div'>Nome do Projeto</h1>
             <h1 className='txt-div'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in quam dapibus, consequat urna scelerisque, tempor est. Sed tempor auctor nisl, feugiat interdum velit hendrerit ut. </h1>
             <h1 className='txt-click'>Click Here →</h1>
+            <div class="back">
+              <p>Descrição do Projeto 1</p>
+            </div>
           </div>
           <div className='proj-2'>
-            <img src={img} className='img'/>
-            <h1 className='title-div'>Nome do Projeto</h1>
-            <h1 className='txt-div'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in quam dapibus, consequat urna scelerisque, tempor est. Sed tempor auctor nisl, feugiat interdum velit hendrerit ut. </h1>
-            <h1 className='txt-click'>Click Here →</h1>
+            <div className='front'>
+              <img src={img} className='img'/>
+              <h1 className='title-div'>Nome do Projeto</h1>
+              <h1 className='txt-div'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in quam dapibus, consequat urna scelerisque, tempor est. Sed tempor auctor nisl, feugiat interdum velit hendrerit ut. </h1>
+              <h1 className='txt-click'>Click Here →</h1>
+            </div>
+            <div class="back">
+              <p>Descrição do Projeto 1</p>
+            </div>
           </div>
         </div>
         <div className='fil-2'>
-          <div className='proj-3'>
-            <img src={img} className='img'/>
-            <h1 className='title-div'>Nome do Projeto</h1>
-            <h1 className='txt-div'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in quam dapibus, consequat urna scelerisque, tempor est. Sed tempor auctor nisl, feugiat interdum velit hendrerit ut. </h1>
-            <h1 className='txt-click'>Click Here →</h1>
+        <div className='proj-3'>
+             <div className='front'>
+                <img src={img} className='img'/>
+                <h1 className='title-div'>Nome do Projeto</h1>
+                <h1 className='txt-div'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in quam dapibus, consequat urna scelerisque, tempor est. Sed tempor auctor nisl, feugiat interdum velit hendrerit ut. </h1>
+                <h1 className='txt-click'>Click Here →</h1>
+              </div>
+              <div class="back">
+              <p>Descrição do Projeto 1</p>
+            </div>
           </div>
           <div className='proj-4'>
-            <img src={img} className='img'/>
-            <h1 className='title-div'>Nome do Projeto</h1>
-            <h1 className='txt-div'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in quam dapibus, consequat urna scelerisque, tempor est. Sed tempor auctor nisl, feugiat interdum velit hendrerit ut. </h1>
-            <h1 className='txt-click'>Click Here →</h1>
+            <div className='front'>
+                <img src={img} className='img'/>
+                <h1 className='title-div'>Nome do Projeto</h1>
+                <h1 className='txt-div'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in quam dapibus, consequat urna scelerisque, tempor est. Sed tempor auctor nisl, feugiat interdum velit hendrerit ut. </h1>
+                <h1 className='txt-click'>Click Here →</h1>
+              </div>
+              <div class="back">
+              <p>Descrição do Projeto 1</p>
+            </div>
           </div>
         </div>
       </section>
