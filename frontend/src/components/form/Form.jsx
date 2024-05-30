321caa9a90b66c9036406a55f488e6cba834af0c
gi-colucci <gcolucci2828@gmail.com>
2024-05-29 21:10:36 -0300

    last updates

---
+++ frontend/src/components/form/Form.jsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+import { sendCustomEmail } from './email';
+import './Form.scss'
+import axios from 'axios';
+import ScrollReveal from 'scrollreveal';
+
+export function Form() {
+    // useEffect(() => {
+    //     ScrollReveal().reveal('.container-form ',
+    //     {
+    //     origin: 'left', 
+    //     distance: '100px', 
+    //     duration: 1000,
+    //     delay: 0, 
+    //     easing: 'ease-out', 
+    //     opacity: 0, 
+    //     scale: 1,
+    //     reset: false, 
+    //   });
+    // }, []);
+
+    const [detalhes, setDatalhes] = useState({
+        email: '',
+        message: '',
+      });
+    
+      const handleDetalhes = (e) => {
+        const { name, value } = e.target;
+    
+        setDatalhes((prevDetalhes) => {
+          return {
+            ...prevDetalhes,
+            [name]: value,
+          };
+        });
+      };
+    
+      const handleSendEmail = () => {
+        sendCustomEmail(detalhes);
+      };
+
+    return (
+        <div className='container-form'>
+            <h2 id="contact"></h2>
+            <h2 className='title'>Vamos trabalhar juntos?</h2>
+            <h2 className='title'>Entre em contato</h2>
+
+            <form>
+                <div className='input-group'>
+                    <input
+                        name="name"
+                        value={detalhes.name}
+                        onChange={handleDetalhes}
+                        type="text"
+                        placeholder="name"
+                    />
+                </div>
+
+                <div className='input-group'>
+                    <input
+                        name="email"
+                        value={detalhes.email}
+                        onChange={handleDetalhes}
+                        type="email"
+                        placeholder="email"
+                    />
+                </div>
+
+                <div className='input-group-msg'>
+                    <input
+                       name="message"
+                       value={detalhes.message}
+                       onChange={handleDetalhes}
+                       type="text"
+                       placeholder="mensagem"
+                    />
+                </div>
+                
+                <div className='btn-submit'>
+                    <button onClick={handleSendEmail}>Enviar</button>
+                </div>
+            </form>
+        </div>
+    );
+}
---
+++ frontend/src/components/form/email.jsx
@@ -0,0 +1,23 @@
+import emailjs from '@emailjs/browser'
+
+
+const sendCustomEmail = (detalhes)=>{
+  emailjs.init(import.meta.env.VITE_EMAIL_USER_ID);
+  emailjs
+    .send(
+      import.meta.env.VITE_EMAIL_SERVICE_ID,
+      import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+      {
+        name:detalhes.name,
+        email:detalhes.email,
+        mensagem:detalhes.message,
+      }
+    )
+    .then((response)=>{
+      console.log(response)
+    })
+    .catch((error)=>{
+      console.log(error)
+    });
+}
+export {sendCustomEmail}
